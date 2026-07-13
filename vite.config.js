cd5a3f3c8692adcef9071fbfd574f334a367c8bc
chayote <trumbing@gmail.com>
2026-07-13 21:17:16 +0900

    todo 앱

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
   // ⚠️ GitHub Pages 배포 시 반드시 아래 값을 본인의 저장소 이름으로 바꿔주세요.
   // 예: 저장소 주소가 https://github.com/yourname/my-todo-app 이라면
   // base: '/my-todo-app/' 로 설정합니다.
-  base: '/YOUR_REPO_NAME/',
-})
+  base: '/todos/',
+});
